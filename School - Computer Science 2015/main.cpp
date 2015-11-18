ecbc9fe273a5c19d749bb6cb0d694f222fb98b33
Kay Gonschior <kay.gonschior@googlemail.com>
2015-11-18 22:15:36 +0100

    add SFML&TGUI

--- School - Computer Science 2015/main.cpp
+++ School - Computer Science 2015/main.cpp
@@ -1,11 +1,37 @@
 #include "stdafx.h"
 using namespace std;
+using namespace sf;
+using namespace tgui;
 
 int main()
 {
+	RenderWindow window( VideoMode( 800, 600 ), "Informatik Projekt" );
+	Gui gui( window );
+
+	gui.setFont( "Data/DejaVuSans.ttf" );
+
 	try
 	{
 		//application
+		while( window.isOpen() )
+		{
+			sf::Event event;
+			while( window.pollEvent( event ) )
+			{
+				if( event.type == sf::Event::Closed )
+					window.close();
+
+				// Pass the event to all the widgets
+				gui.handleEvent( event );
+			}
+
+			window.clear();
+
+			// Draw all created widgets
+			gui.draw();
+
+			window.display();
+		}
 	}
 	catch( const std::exception& e )
 	{
--- School - Computer Science 2015/stdafx.h
+++ School - Computer Science 2015/stdafx.h
@@ -1,3 +1,12 @@
 #pragma once
 
 #include <iostream>
+
+#include<SFML/Audio.hpp>
+#include<SFML/Graphics.hpp>
+#include<SFML/Main.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Network.hpp>
+#include<SFML/Window.hpp>
+
+#include <TGUI/TGUI.hpp>
